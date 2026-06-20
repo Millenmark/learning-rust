a668cdc2831c88c4d5d767585a4c2a352381487d
Millenmark <millenmark.dev@gmail.com>
2026-06-20 15:17:59 +0800

    behaviour of variable within a scope | variable shadowing

--- doc-variables/src/main.rs
+++ doc-variables/src/main.rs
@@ -4,4 +4,19 @@ fn main() {
     println!("The value of x is {x}");
     x = 6;
     println!("The value of x is {x}");
+
+    let a = 5;
+
+    let a = a + 1;
+
+    println!("The value of a after increment and before the inner scope is: {a}");
+
+    {
+        let a = a * 2;
+        println!("The value of a in the inner scope is: {a}");
+    }
+
+    // let a = 56;
+
+    println!("The value of a in the outer scope is: {a}");
 }
